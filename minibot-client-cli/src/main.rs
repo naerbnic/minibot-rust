ea15acb8b6b6820dec2bfe7c1a7fde25f74d0ddf
Brian Chin <brian.chin@gmail.com>
2020-04-06 23:45:26 -0700

    Add first version of the client cli

--- minibot-client-cli/src/main.rs
+++ minibot-client-cli/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = reqwest::Client::new();
+
+    let (url, future) = minibot_client::run_client(&client, std::time::Instant::now() + std::time::Duration::from_secs(300), "", "");
+
+    println!("Output: {:?}", webbrowser::open(&url.to_string())?);
+
+    let access_token = future.await?;
+
+    println!("Access token: {}", access_token);
+
+    Ok(())
 }
