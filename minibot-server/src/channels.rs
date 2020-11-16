d2460ba1f65d891a405efa78919c8aed7e54994c
Brian Chin <brian.chin@gmail.com>
2020-11-16 17:57:55 +0000

    Add async functions to stream serde types

--- minibot-server/src/channels.rs
+++ minibot-server/src/channels.rs
@@ -31,22 +31,22 @@ impl ChannelAcceptor {
         &self,
         conn: T,
     ) -> anyhow::Result<()> {
-        let (sink, stream) = conn.split();
+        let (_sink, _stream) = conn.split();
 
-        let (error_send, error_recv) = mpsc::channel(1);
-        let (server_send, server_recv) = mpsc::channel(10);
+        // let (error_send, error_recv) = mpsc::channel(1);
+        // let (server_send, server_recv) = mpsc::channel(10);
 
-        let stream = stream.map(|msg| match msg {
-            WsMessage::Text(text) => Ok::<_, anyhow::Error>(
-                serde_json::from_str(&text).context("Could not parse rpc message")?,
-            ),
+        // let stream = stream.map(|msg| match msg {
+        //     WsMessage::Text(text) => Ok::<_, anyhow::Error>(
+        //         serde_json::from_str(&text).context("Could not parse rpc message")?,
+        //     ),
 
-            WsMessage::Binary(_) => Err(anyhow::anyhow!("Unexpected binary message")),
-        });
+        //     WsMessage::Binary(_) => Err(anyhow::anyhow!("Unexpected binary message")),
+        // });
 
-        let server_recv = futures::stream::select(server_recv, error_recv);
+        // let server_recv = futures::stream::select(server_recv, error_recv);
 
-        ClientChannel::start_channel(stream, sink, spawner, handler);
+        // ClientChannel::start_channel(stream, sink, spawner, handler);
 
         todo!();
     }
--- minibot-server/src/util/future/mod.rs
+++ minibot-server/src/util/future/mod.rs
@@ -3,6 +3,7 @@ pub mod opt_cell;
 pub mod park;
 
 use futures::prelude::*;
+use serde::{de::DeserializeOwned, Serialize};
 
 pub async fn send_all_propagate<In, Out>(stream: In, mut sink: Out) -> Result<(), Out::Error>
 where
@@ -10,4 +11,69 @@ where
     Out: Sink<In::Item> + Unpin,
 {
     sink.send_all(&mut stream.map(Result::Ok)).await
+}
+
+#[derive(Copy, Clone, thiserror::Error, Debug)]
+pub enum PatchError<I, O>
+where
+    I: std::error::Error + 'static,
+    O: std::error::Error + 'static,
+{
+    #[error("Error from source: {0:?}")]
+    Stream(#[source] I),
+    #[error("Error from sink: {0:?}")]
+    Sink(#[source] O),
+}
+
+pub async fn send_all_until_error<In, Out>(
+    stream: In,
+    mut sink: Out,
+) -> Result<(), PatchError<In::Error, Out::Error>>
+where
+    In: TryStream + Unpin,
+    Out: Sink<In::Ok> + Unpin,
+    In::Error: std::error::Error + 'static,
+    Out::Error: std::error::Error + 'static,
+{
+    let mut stream = stream.into_stream();
+    while let Some(try_item) = stream.next().await {
+        match try_item {
+            Ok(item) => match sink.send(item).await {
+                Ok(()) => {}
+                Err(e) => return Err(PatchError::Sink(e)),
+            },
+            Err(e) => return Err(PatchError::Stream(e)),
+        }
+    }
+
+    Ok(())
+}
+
+/// Link up a stream and a sink, where the stream messages are deserialized and passed along.
+pub async fn deser_json_stream<In, Out, Item, T>(
+    stream: In,
+    sink: Out,
+) -> Result<(), PatchError<serde_json::Error, Out::Error>>
+where
+    In: Stream<Item = Item> + Unpin,
+    Item: std::borrow::Borrow<str>,
+    T: DeserializeOwned,
+    Out: Sink<T> + Unpin,
+    Out::Error: std::error::Error + 'static,
+{
+    send_all_until_error(stream.map(|item| serde_json::from_str(item.borrow())), sink).await
+}
+
+/// Link up a stream and a sink, where the stream messages are serialized and passed along.
+pub async fn ser_json_stream<In, Out, T>(
+    stream: In,
+    sink: Out,
+) -> Result<(), PatchError<serde_json::Error, Out::Error>>
+where
+    In: Stream<Item = T> + Unpin,
+    T: Serialize,
+    Out: Sink<String> + Unpin,
+    Out::Error: std::error::Error + 'static,
+{
+    send_all_until_error(stream.map(|item| serde_json::to_string(&item)), sink).await
 }
