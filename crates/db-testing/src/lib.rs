2655218b3922fc03be9148a4ed883805ce88cbbb
Brian Chin <brian.chin@gmail.com>
2020-12-19 21:50:47 -0800

    Clean up unused features in db-testing/lib.rs

--- crates/db-testing/src/lib.rs
+++ crates/db-testing/src/lib.rs
@@ -1,18 +1,5 @@
 pub mod docker;
 
-use nix::sys::signal::kill;
-use std::process::{Child, Command, Stdio};
-use std::time::{Duration, Instant};
-use std::{convert::TryInto, path::Path};
-use std::{
-    io::{self, prelude::*},
-    thread::{sleep, spawn},
-};
-
-use regex::Regex;
-
-use tempdir::TempDir;
-
 use minibot_db_postgres::DbHandle;
 
 #[derive(thiserror::Error, Debug)]
@@ -21,65 +8,10 @@ pub enum Error {
     Db(#[from] minibot_db_postgres::Error),
 }
 
-fn read_container_id(deadline: Instant, path: &Path) -> io::Result<String> {
-    loop {
-        if Instant::now() > deadline {
-            return Err(io::Error::new(
-                io::ErrorKind::TimedOut,
-                "Unable to open file before deadline.",
-            ));
-        }
-
-        match std::fs::read_to_string(path) {
-            Ok(str) => {
-                if str.len() == 64 {
-                    return Ok(str);
-                }
-            }
-            Err(e) => {
-                if !matches!(e.kind(), io::ErrorKind::NotFound) {
-                    return Err(e);
-                }
-            }
-        }
-
-        sleep(Duration::from_millis(100));
-    }
-}
-
-fn get_docker_port(id: &str, expected_inner_port: u16) -> anyhow::Result<u16> {
-    let output = Command::new("docker").arg("port").arg(id).output()?;
-
-    anyhow::ensure!(
-        output.status.success(),
-        "docker port command failed: {:?}",
-        output.status
-    );
-
-    let port_re = Regex::new(r"^(\d+)/([[:alpha:]]+) -> ([^:]+):(\d+)$").unwrap();
-
-    for line in std::str::from_utf8(&output.stdout).unwrap().lines() {
-        let cap = port_re.captures(line).unwrap();
-        let inner_port: u16 = cap[1].parse()?;
-        let _protocol = &cap[2];
-        let _ext_hostname = &cap[3];
-        let ext_port: u16 = cap[4].parse()?;
-
-        if inner_port == expected_inner_port {
-            return Ok(ext_port);
-        }
-    }
-
-    anyhow::bail!(
-        "Could not find docker binding for port {}",
-        expected_inner_port
-    );
-}
-
 pub struct TestDb {
     port: u16,
     password: Option<String>,
-    process: docker::Process,
+    _process: docker::Process,
 }
 
 impl TestDb {
@@ -105,8 +37,6 @@ impl TestDb {
             .exit_signal(docker::Signal::Quit)
             .start()?;
 
-        let deadline = Instant::now() + Duration::from_secs(30);
-
         // Wait for the database to be ready
         let mut ext_port = None;
         for port in process.port_bindings()? {
@@ -125,7 +55,7 @@ impl TestDb {
         Ok(TestDb {
             port: ext_port,
             password: Some("postgres".to_string()),
-            process,
+            _process: process,
         })
     }
 
